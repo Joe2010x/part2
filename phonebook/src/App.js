6f678712ddeffb77d89f352bbde0e3ac1f3205ac
Dongping Sun <dongping.d.sun@utu.fi>
2021-10-10 00:44:01 +0300

    part 2.6

---
+++ phonebook/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+ 
+  const handleChange =(event) =>{
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleClick =(event) => {
+    event.preventDefault();
+   // console.log(event.target)
+    setPersons(persons.concat({ name: newName }));
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      {/* <div>debug: {newName}</div> */}
+      <form>
+        <div>
+          name:
+          <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button onClick={handleClick} type="submit">
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
