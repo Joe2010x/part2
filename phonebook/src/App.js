91d1f5722b7d562782202f6080d6189777f0cb8b
Dongping Sun <dongping.d.sun@utu.fi>
2021-10-10 12:22:38 +0300

    part 2.11

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter.js";
 import PersonForm from "./components/PersonForm.js";
 import Persons from "./components/Persons.js";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  
+const [persons, setPersons] = useState([]);
+  useEffect (()=>{
+    axios.get("http://localhost:3001/persons")
+      .then (promise =>{
+        //console.log("returned from promise ",promise.data);
+        setPersons(promise.data);
+      })
+  },[])
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
@@ -39,7 +44,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>debug: {filter}</div>
+      {/* <div>debug: {filter}</div> */}
 
       <Filter onChange={handleFilter} value={filter} />
 
